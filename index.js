0ca952bd25669af75545fb37a45d7feb3e06da84
Jim <rocket.fish@mail.com>
2020-09-29 09:46:00 -0400

    change default port to 8000, in line with documentation

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const { ensureLoggedOut, ensureLoggedIn } = require("connect-ensure-login");
  *
  */
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 /**
  * Making sure that the required environment variables are filled out
